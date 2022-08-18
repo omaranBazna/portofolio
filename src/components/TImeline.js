952d2878bc8db4de0e5a50fa0822b504330665f0
omaran bazna <omran4d@gmail.com>
2022-08-18 11:29:23 -0400

    create Technology component

--- src/components/TImeline.js
+++ src/components/TImeline.js
@@ -1,11 +1,13 @@
 import "./styles/time-line.css";
-
+import Technology from "./Technology";
 import React from "react";
 
 const TImeline = () => {
   return (
     <div className="time-line">
-      <div className="whole-time-line"></div>
+      <div className="whole-time-line">
+        <Technology></Technology>
+      </div>
     </div>
   );
 };
---
+++ src/components/Technology.js
@@ -0,0 +1,8 @@
+import "./styles/technology.css";
+import React from "react";
+
+const Technology = () => {
+  return <div></div>;
+};
+
+export default Technology;
