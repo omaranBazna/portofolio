d3e55ad001e3a8ac8e011c729f0c686569f8cec5
omaran bazna <omran4d@gmail.com>
2022-08-18 04:51:23 -0400

    add github icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import "./styles/footer.css";
 
 import React from "react";
-
+import { Button } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="social"></div>
+      <div className="social">
+        <Button>
+          <GitHubIcon className="github-icon" />
+        </Button>
+      </div>
       <div className="contact-me"></div>
       <div className="resume"></div>
     </div>
