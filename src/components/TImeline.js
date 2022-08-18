83c205db18489103ca2720cdf1e117e8f366d00e
omaran bazna <omran4d@gmail.com>
2022-08-18 11:27:46 -0400

    whole-time-line

--- src/components/TImeline.js
+++ src/components/TImeline.js
@@ -3,7 +3,11 @@ import "./styles/time-line.css";
 import React from "react";
 
 const TImeline = () => {
-  return <div className="time-line"></div>;
+  return (
+    <div className="time-line">
+      <div className="whole-time-line"></div>
+    </div>
+  );
 };
 
 export default TImeline;
