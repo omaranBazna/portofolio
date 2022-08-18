10ad4e97523c9b2a6ff51907c6a440b42bbf06e4
omaran bazna <omran4d@gmail.com>
2022-08-18 04:49:25 -0400

    add material icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import "./styles/footer.css";
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="social"></div>
+      <div className="contact-me"></div>
+      <div className="resume"></div>
+    </div>
+  );
 };
 
 export default Footer;
