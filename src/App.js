dc2c6c64c645ec824d078b05335b0743a9b25bb1
omaran bazna <omran4d@gmail.com>
2022-08-18 05:29:35 -0400

    add hero section content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div className="App">
+      <HeroSection />
       <Footer />
     </div>
   );
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,20 @@ import "./styles/hero-section.css";
 import React from "react";
 
 const HeroSection = () => {
-  return <div className="hero-section"></div>;
+  return (
+    <div className="hero-section">
+      <div className="hero-image">
+        <img src="/images/omaran.jpg" />
+      </div>
+      <div className="hero-text">
+        <p>
+          Hello Guys , I am omaran bazna ,React JS developer I have been working
+          for +4 years I have experience in varies technologies Thank for
+          visiting my website
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
