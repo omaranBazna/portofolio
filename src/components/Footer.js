5ac3231a86ab1ff24df4df427312a84562061e94
omaran bazna <omran4d@gmail.com>
2022-08-18 04:59:48 -0400

    add linked in Icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,16 @@ import "./styles/footer.css";
 import React from "react";
 import { Button } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 const Footer = () => {
   return (
     <div className="footer">
       <div className="social">
         <Button>
           <GitHubIcon className="github-icon" />
+        </Button>
+        <Button>
+          <LinkedInIcon className="linked-in-icon" />
         </Button>
       </div>
       <div className="contact-me"></div>
