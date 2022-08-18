d33408f541568173fec5c9826170438982773c1f
omaran bazna <omran4d@gmail.com>
2022-08-18 04:47:12 -0400

    add the footer to the main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
