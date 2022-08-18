a849317c1d6714b9956d4e5eecf78c8c4288dcde
omaran bazna <omran4d@gmail.com>
2022-08-18 11:12:28 -0400

    add hero content

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,15 +5,17 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="hero-section">
-      <div className="hero-image">
-        <img src="/images/omaran.jpg" />
-      </div>
-      <div className="hero-text">
-        <p>
-          Hello Guys , I am omaran bazna ,React JS developer I have been working
-          for +4 years I have experience in varies technologies Thank for
-          visiting my website
-        </p>
+      <div className="hero-content">
+        <div className="hero-image">
+          <img src="/images/omaran.jpg" />
+        </div>
+        <div className="hero-text">
+          <p>
+            Hello Guys , I am omaran bazna ,React JS developer I have been
+            working for +4 years I have experience in varies technologies Thank
+            for visiting my website
+          </p>
+        </div>
       </div>
     </div>
   );
