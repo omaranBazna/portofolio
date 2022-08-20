f58ef8f8cab59dbe2472940530cd247e47045164
omaran bazna <omran4d@gmail.com>
2022-08-20 02:10:49 -0400

    styling the time line

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import "./App.css";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Timeline from "./components/Timeline";
-
+import { useState, useEffect } from "react";
 function App() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset / window.outerHeight;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className="App">
       <HeroSection />
-      <Timeline />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <Timeline scroll={scrollPosition} />
       <Footer />
     </div>
   );
--- src/components/TImeline.js
+++ src/components/TImeline.js
@@ -2,14 +2,38 @@ import "./styles/time-line.css";
 import Technology from "./Technology";
 import React from "react";
 
-const Timeline = () => {
+const Timeline = ({ scroll }) => {
   return (
     <div className="time-line">
+      <div
+        style={{
+          width: "7px",
+          height: `${scroll * 90}%`,
+          background: "white",
+          position: "absolute",
+          top: "00%",
+          transform: `translate(-1px,-${0}%)`,
+          borderRadius: "2px 2px 2px 2px",
+        }}
+      />
+
       <div className="whole-time-line">
-        <img src="/images/react-js-icon.png" />
-        <img src="/images/Unity.png" />
-        <img src="/images/teacher.png" />
-        <img src="/images/certificate.png" />
+        <img
+          src="/images/react-js-icon.png"
+          className={scroll > 0 && scroll < 0.25 ? "active" : "in-active"}
+        />
+        <img
+          src="/images/Unity.png"
+          className={scroll > 0.25 && scroll < 0.5 ? "active" : "in-active"}
+        />
+        <img
+          src="/images/teacher.png"
+          className={scroll > 0.5 && scroll < 0.75 ? "active" : "in-active"}
+        />
+        <img
+          src="/images/certificate.png"
+          className={scroll > 0.75 && scroll < 1 ? "active" : "in-active"}
+        />
       </div>
     </div>
   );
