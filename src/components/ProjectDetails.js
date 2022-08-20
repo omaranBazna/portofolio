4a6d3dc0c71bfaf00579b433450e8171e07281e4
omaran bazna <omran4d@gmail.com>
2022-08-20 17:33:41 -0400

    add skills to the project

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -8,8 +8,9 @@ const ProjectDetails = () => {
       <img src="https://i.pinimg.com/originals/0b/bf/c7/0bbfc7ccf6eda7d4173eb795526de3a9.jpg"></img>
       <p>
         In this Project ,I cloned the official website of{" "}
-        <a>Walt Disney Movies </a> using
+        <a>Walt Disney Movies </a> using :
       </p>
+      <div className="skills"></div>
     </div>
   );
 };
