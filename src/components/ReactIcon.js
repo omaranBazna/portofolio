8fec3d35087b6767a7695425d46a3ce12bb5cf82
omaran bazna <omran4d@gmail.com>
2022-08-20 02:28:32 -0400

    style the react icon

--- src/components/ReactIcon.js
+++ src/components/ReactIcon.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./styles/react-icon.css";
 const ReactIcon = () => {
   return (
-    <div>
+    <div className="react-icon">
       <img src="/images/react-js-icon.png" />
     </div>
   );
