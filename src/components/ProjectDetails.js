f88a24077564642f45732cc7dae4ca56c3256de7
omaran bazna <omran4d@gmail.com>
2022-08-20 17:32:29 -0400

    add some details to the project description

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -6,6 +6,10 @@ const ProjectDetails = () => {
   return (
     <div className="project-details">
       <img src="https://i.pinimg.com/originals/0b/bf/c7/0bbfc7ccf6eda7d4173eb795526de3a9.jpg"></img>
+      <p>
+        In this Project ,I cloned the official website of{" "}
+        <a>Walt Disney Movies </a> using
+      </p>
     </div>
   );
 };
