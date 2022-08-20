190d72fc9454802569a7d27d2915d631c0bcba87
omaran bazna <omran4d@gmail.com>
2022-08-20 17:29:00 -0400

    add cover image

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -3,7 +3,11 @@ import "./styles/project-details.css";
 import React from "react";
 
 const ProjectDetails = () => {
-  return <div className="project-details"></div>;
+  return (
+    <div className="project-details">
+      <img src="https://i.pinimg.com/originals/0b/bf/c7/0bbfc7ccf6eda7d4173eb795526de3a9.jpg"></img>
+    </div>
+  );
 };
 
 export default ProjectDetails;
