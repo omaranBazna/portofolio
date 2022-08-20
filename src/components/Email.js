61d4cb8fc5e675146784026ad19befea0cea4143
omaran bazna <omran4d@gmail.com>
2022-08-20 10:24:59 -0400

    import component

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 import "./styles/email.css";
 const Email = () => {
   return <div></div>;
