93e10fb834eb176b430d05a56805a135acfc1e51
omaran bazna <omran4d@gmail.com>
2022-08-20 02:24:48 -0400

    add more description to the disney project

--- src/components/ReactProjectCard.js
+++ src/components/ReactProjectCard.js
@@ -4,7 +4,10 @@ const ReactProjectCard = () => {
   return (
     <div>
       <img src="https://www.disneychannel.ca/wp-content/themes/disney-child/assets/images/thumbnail-generic.jpg" />
-      <p>Description</p>
+      <p>
+        This website is a clone to the real Disney website build using
+        ReactJS,Styled components and firebase
+      </p>
     </div>
   );
 };
