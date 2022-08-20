ed81968f4678778098315ff617c16c20ec645e4b
omaran bazna <omran4d@gmail.com>
2022-08-20 12:10:42 -0400

    finish pop up successful message

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -3,7 +3,10 @@ import "./styles/popup.css";
 const PopUp = () => {
   return (
     <div className="popUp">
-      <div>your mail was send successfully </div>
+      <div className="message-body">
+        your message was sent successfully
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7RjzpwD44bg-UZlrqT3ARkiibAhAMvxxdx6gxysU&s" />
+      </div>
     </div>
   );
 };
