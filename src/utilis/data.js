803cefa25a92ffa5f5bb05bfcf84b460110cc305
omaran bazna <omran4d@gmail.com>
2022-08-22 01:59:28 -0400

    add project 2

--- src/utilis/data.js
+++ src/utilis/data.js
@@ -18,6 +18,25 @@ export const ReactProjects = [
     github: "https://github.com/omaranBazna/disney-clone",
     url: "https://omaranbazna.github.io/disney-clone/",
   },
+  {
+    id: "1",
+    name: "LinkedIn clone",
+    photo:
+      "https://pryormediacdn.azureedge.net/wordpress/2016/04/No-Hype-Social-Media-for-Business-LinkedIn.jpg",
+    description:
+      "This website is a clone to the real LinkedIn build using ReactJS,Styled components and firebase",
+    detailsDescription:
+      "In this Project ,I cloned the official website of LinkedIn using :",
+    skills: [
+      "ReactJS",
+      "ReactRouter",
+      "ReactRedux",
+      "StyledComponents",
+      "Firebase",
+    ],
+    github: "https://github.com/omaranBazna/linked-in-clone2",
+    url: "https://omaranbazna.github.io/linked-in-clone2/",
+  },
 ];
 
 export const skills = {
