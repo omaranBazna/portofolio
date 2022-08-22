bec333eeeedf64fceea73d4c1dadf075f62eb3b7
omaran bazna <omran4d@gmail.com>
2022-08-22 01:24:57 -0400

    create window slice

---
+++ src/app/features/windowSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
+
+const initialState = {
+  showMessage: false,
+  showProjectDetails: false,
+};
+
+export const windowSlice = createSlice({
+  name: "windows",
+  initialState,
+  reducers: {
+    openMessage: (state, action) => {
+      state.showMessage = false;
+    },
+    closeMessage: (state) => {
+      state.showMessage = false;
+    },
+    openProjectD: (state) => {
+      state.showProjectDetails = true;
+    },
+    closeProjectD: (state) => {
+      state.showProjectDetails = false;
+    },
+  },
+});
+export const windowReducer = windowSlice.reducer;
+export const { openMessage, closeMessage, openProjectD, closeProjectD } =
+  windowSlice.actions;
