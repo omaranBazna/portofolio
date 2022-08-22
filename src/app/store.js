086c2283a3017fb7254e9c18014de128a821acf6
omaran bazna <omran4d@gmail.com>
2022-08-22 01:27:29 -0400

    add redux to the project

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { windowReducer } from "./features/windowSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    windows: windowReducer,
+  },
 });
