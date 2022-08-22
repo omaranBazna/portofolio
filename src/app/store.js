750f537f9d64054f47484f9853e68f686f3cbbd5
omaran bazna <omran4d@gmail.com>
2022-08-22 02:14:22 -0400

    import the project slice to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { windowReducer } from "./features/windowSlice";
+import { projectReducer } from "./features/projectSlice";
 export const store = configureStore({
   reducer: {
     windows: windowReducer,
+    project: projectReducer,
   },
 });
