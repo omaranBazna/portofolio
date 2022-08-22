725268e9d684ee6b737ef69fcf34af590892f23f
omaran bazna <omran4d@gmail.com>
2022-08-22 01:52:11 -0400

    add skills link

--- src/utilis/data.js
+++ src/utilis/data.js
@@ -8,8 +8,22 @@ export const ReactProjects = [
       "This website is a clone to the real Disney website build using ReactJS,Styled components and firebase",
     detailsDescription:
       "In this Project ,I cloned the official website of Walt Disney Movies using :",
-    skills: [ReactJS, ReactRouter, ReactRedux, StyledComponents, Firebase],
+    skills: [
+      "ReactJS",
+      "ReactRouter",
+      "ReactRedux",
+      "StyledComponents",
+      "Firebase",
+    ],
     github: "https://github.com/omaranBazna/disney-clone",
     url: "https://omaranbazna.github.io/disney-clone/",
   },
 ];
+
+export const skills = {
+  ReactJs: "https://reactjs.org/",
+  ReactRouter: "https://reactrouter.com/",
+  ReactRedux: "https://react-redux.js.org/",
+  StyledComponents: "https://styled-components.com/",
+  Firebase: "https://firebase.google.com/",
+};
