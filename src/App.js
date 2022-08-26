4bf6db586d0991869e44797d4b5c0ae16f3f8930
omaran bazna <omran4d@gmail.com>
2022-08-25 23:39:17 -0400

    add React native porto

--- src/App.js
+++ src/App.js
@@ -1,42 +1,28 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
-import Timeline from "./components/Timeline";
+
 import { useState, useEffect } from "react";
 import ReactProto from "./components/ReactPorto";
+import ReactNativePorto from "./components/ReactNativePorto";
 import Email from "./components/Email";
 import PopUpSucc from "./components/PopUpSucc";
 import PopUpFail from "./components/PopUpFail";
 import ProjectDetails from "./components/ProjectDetails";
 
 function App() {
-  const [scrollPosition, setScrollPosition] = useState(0);
   const [showEmailForm, setShowEmailForm] = useState(false);
   const [popUpSuc, setPopUpSuc] = useState(false);
   const [popUpFail, setPopUpFail] = useState(false);
   const [showProjectDetails, setShowProjectDetails] = useState(false);
-  const handleScroll = () => {
-    const position = window.pageYOffset / window.outerHeight;
-    setScrollPosition(position);
-  };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   return (
     <div className="App">
       <HeroSection />
-      <ReactProto scroll={scrollPosition} setShow={setShowProjectDetails} />
+      <ReactProto setShow={setShowProjectDetails} />
+      <ReactNativePorto setShow={setShowProjectDetails} />
       {showProjectDetails && <ProjectDetails setShow={setShowProjectDetails} />}
-      {/*
-      
-   
-      <Timeline scroll={scrollPosition} />
-  */}
+
       {popUpSuc && <PopUpSucc setPop={setPopUpSuc} />}
       {popUpFail && <PopUpFail setPop={setPopUpFail} />}
       {showEmailForm && (
