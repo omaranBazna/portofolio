7ed91afbcc7e639ef9e6248b748de7d6ceace099
omaran bazna <omran4d@gmail.com>
2022-08-28 01:16:41 -0400

    divide the hero to left and right

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,17 +6,20 @@ const HeroSection = () => {
   return (
     <div className="hero-section">
       <div className="hero-content">
-        <div className="hero-image">
-          <img src="/images/omaran.jpg" />
+        <div className="hero-left">
+          <div className="hero-image">
+            <img src="/images/omaran.jpg" />
+          </div>
+          <div className="hero-text">
+            <h1>OMARAN BAZNA</h1>
+            <h3>full stack developer</h3>
+            <p>
+              It is my pleasure to welcome you to my website. Feel free to
+              contact me at any time
+            </p>
+          </div>
         </div>
-        <div className="hero-text">
-          <h1>OMARAN BAZNA</h1>
-          <h4>full stack developer</h4>
-          <p>
-            It is my pleasure to welcome you to my website. Feel free to contact
-            me at any time
-          </p>
-        </div>
+        <div className="hero-right"></div>
       </div>
     </div>
   );
