0b8349fd6a39fb05bfe83434136f438f66204ff4
omaran bazna <omran4d@gmail.com>
2022-09-11 23:39:47 -0400

    add form to the search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const SearchBar = () => {
-  return <div></div>;
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit"></button>
+    </form>
+  );
 };
 
 export default SearchBar;
