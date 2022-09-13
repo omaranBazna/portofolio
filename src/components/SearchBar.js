727166b373988b9c9b71e9ee257e2a59f6ef20e6
omaran bazna <omran4d@gmail.com>
2022-09-13 00:00:08 -0400

    add link to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import "./styles/SearchBar.css";
+import { Link } from "react-router-dom";
 const SearchBar = () => {
   return (
     <form>
       <input type="text" placeholder="Search for skill or project..." />
-      <button type="submit">
-        <SearchIcon />
-      </button>
+      <Link to="/portofolio/main">
+        <button type="submit">
+          <SearchIcon />
+        </button>
+      </Link>
     </form>
   );
 };
