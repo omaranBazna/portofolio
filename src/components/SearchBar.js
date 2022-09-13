acb909bcbda16196bb33668d13a7b309ace80011
omaran bazna <omran4d@gmail.com>
2022-09-12 23:48:50 -0400

    add search icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import SearchIcon from "@mui/icons-material/Search";
 const SearchBar = () => {
   return (
     <form>
       <input type="text" />
-      <button type="submit">Search</button>
+      <button type="submit">
+        <SearchIcon />
+      </button>
     </form>
   );
 };
